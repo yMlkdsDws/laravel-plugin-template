020b301fae284efced1a6b698317e38b1e38f4cc
ii masamitsu <ii@entap.co.jp>
2020-12-07 08:58:17 +0900

    テストケースを用意した

---
+++ tests/TestCase.php
@@ -0,0 +1,28 @@
+<?php
+namespace Tests;
+
+use Entap\ExamplePackage\ServiceProvider;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+  protected function setUp(): void
+  {
+    parent::setUp();
+
+    $this->artisan('vendor:publish', [
+      '--force' => true,
+      '--provider' => ServiceProvider::class,
+    ]);
+  }
+
+  protected function getPackageProviders($app)
+  {
+    return [ServiceProvider::class];
+  }
+
+  protected function getPackageAliases($app)
+  {
+    return [];
+  }
+}
