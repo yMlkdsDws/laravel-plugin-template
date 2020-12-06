c2a20cd5f881090ba597d94e59bc6673d22a0562
ii masamitsu <ii@entap.co.jp>
2020-12-07 08:58:11 +0900

    サービスプロバイダーを用意した

---
+++ src/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Entap\ExamplePackage;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+  public function boot()
+  {
+    //
+  }
+
+  public function register()
+  {
+    //
+  }
+}
