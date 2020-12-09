b5038b75355aa4d493035618903b2c7415abc404
ii masamitsu <ii@entap.co.jp>
2020-12-10 07:21:40 +0900

    コントローラーとビューも初めから用意する

---
+++ src/Application/Controllers/Controller.php
@@ -0,0 +1,14 @@
+<?php
+namespace Entap\ExamplePackage\Application\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    // use AuthorizesRequests;
+// use DispatchesJobs;
+// use ValidatesRequests;
+}
