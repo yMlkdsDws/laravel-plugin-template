11c8d2ef1db8f0c4236365e97843313cee90f10b
ii masamitsu <ii@entap.co.jp>
2020-12-10 07:13:05 +0900

    vendor:publishはコメントアウトした

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,23 +6,23 @@ use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-  protected function setUp(): void
-  {
-    parent::setUp();
+    protected function setUp(): void
+    {
+        parent::setUp();
 
-    $this->artisan('vendor:publish', [
-      '--force' => true,
-      '--provider' => ServiceProvider::class,
-    ]);
-  }
+        // $this->artisan('vendor:publish', [
+        //   '--force' => true,
+        //   '--provider' => ServiceProvider::class,
+        // ]);
+    }
 
-  protected function getPackageProviders($app)
-  {
-    return [ServiceProvider::class];
-  }
+    protected function getPackageProviders($app)
+    {
+        return [ServiceProvider::class];
+    }
 
-  protected function getPackageAliases($app)
-  {
-    return [];
-  }
+    protected function getPackageAliases($app)
+    {
+        return [];
+    }
 }
